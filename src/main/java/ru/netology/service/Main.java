5ee48584c3c23f44fb9e1dd3684e0c11d9fe9b17
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-10-14 22:02:33 +0300

    CashBackServiceTest was changed in the Junit4 branch

--- src/main/java/ru/netology/service/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.service;
-
-public class Main {
-    public static void main(String[] args) {
-        CashBackService service = new CashBackService();
-        int result = service.remain(100);
-        System.out.println(result);
-    }
-}
